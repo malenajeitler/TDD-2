d521f413fab5e9fe963e2d97171443db4bdd5182
malenajeitler <malenajeitler@gmail.com>
2026-05-22 11:49:59 +0200

    TC_3 added Prod Code (2) Malena

--- src/electrons.js
+++ src/electrons.js
@@ -1,5 +1,5 @@
 // Electrons.js
 function Electrons() {}
 Electrons.prototype.core = function(dice_values) {
-    return dice_values[dice_values.length - 1] + 1;
+    return dice_values[dice_values.length - 1] - 1;
 };
