302d5902c1989ada907dc9cb4508f013256c4077
malenajeitler <malenajeitler@gmail.com>
2026-05-22 11:42:59 +0200

    TC_2 added Test Case (1) Malena

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -1,11 +1,16 @@
 describe('Electrons', function() {
 	beforeEach(function() {
-		Electrons = new Electrons();
+		electronsInstance = new Electrons();
 	});
 
 	//TC_1
 	it('should return 6 for [ 1, 2, 3, 4, 5 ]', function() {
-		expect(Electrons.core([1, 2, 3, 4, 5])).toEqual(6);
+		expect(electronsInstance.core([1, 2, 3, 4, 5])).toEqual(6);
+	});
+
+    //TC_2
+    it('should return 4 for [ 2, 2, 3, 3 ]', function() {
+		expect(electronsInstance.core([ 2, 2, 3, 3 ])).toEqual(4);
 	});
 
 	
