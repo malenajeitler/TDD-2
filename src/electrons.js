d4231e3f359a652c3b98645f35458874becb8615
marleneeeeeeeee <ma.haberhauer@gmail.com>
2026-05-22 11:56:17 +0200

    TC_4 added Prod Code (2) Marlene

--- src/electrons.js
+++ src/electrons.js
@@ -3,7 +3,12 @@ function Electrons() {}
 Electrons.prototype.core = function(dice_values) {
     var result = dice_values[dice_values.length - 1] ;
     if(dice_values.length > 5) {
-        result -= 1;
+        if(result === 3) {
+            result -= 1;
+        }
+        else {
+            result += 10;
+        }
     }
     else {
         result += 1;
